e868b335098a5e06d1c3faa7185f2269bb47852c
ltp1405 <lamtungpro1405@gmail.com>
2026-04-23 22:41:33 +0700

    New frontend

--- frontend/src/pages/SignupPage.jsx
+++
@@ -1,5 +0,0 @@
-import SignupForm from '../components/auth/SignupForm';
-
-const SignupPage = () => <SignupForm />;
-
-export default SignupPage;
---
+++ frontend/src/routes/signup/index.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router"
+
+export const Route = createFileRoute("/signup/")({ component: SignUpPage })
+
+function SignUpPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Signup for new account</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full">
+            Signup
+          </Button>
+          <div className="flex w-full flex-row items-center justify-between">
+            <span>Already have an account?</span>
+            <Button variant="link">
+              <Link to="/login">Login</Link>
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
